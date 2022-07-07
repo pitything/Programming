4d4f84c5bdab97d08cb82ed921a97d3742984f72
pitything <826384739@qq.com>
2022-07-07 23:58:54 +0800

    2022-07-07记录

---
+++ Java/JavaSE/第11章_Java集合/TestCollection.java
@@ -0,0 +1,235 @@
+import org.junit.Test;
+
+import java.util.*;
+
+public class TestCollection {
+    @Test
+    public void testCollection(){
+        /** Collection */
+        System.out.println("-----------Collection-----------");
+        Collection<Integer> collection1 = new ArrayList<>();
+        // 1.添加
+        collection1.add(1);
+        collection1.add(2);
+        collection1.add(3);
+        collection1.add(4);
+        collection1.add(5);
+        collection1.addAll(new ArrayList<Integer>(){{add(6);}});
+        System.out.println(collection1); // [1, 2, 3, 4, 5, 6]
+        // 2.获取有效元素的个数:6
+        System.out.println(collection1.size());
+        // 3.是否包含某个元素,用元素的equals方法来判断，拿两个集合的元素挨个比较
+        System.out.println(collection1.contains(3));                                      // true
+        System.out.println(collection1.containsAll(new ArrayList<Integer>() {{add(3);}}));// true
+        // 4.删除：通过元素的equals方法判断是否是要删除的那个元素。只会删除找到的第一个元素
+        System.out.println(collection1.remove(3)); // true
+        System.out.println(collection1.removeAll(new ArrayList<Integer>() {{add(2);}})); // true
+        // 5.把交集的结果存在当前集合中，不影响其他集合
+        System.out.println(collection1.retainAll(new ArrayList<Integer>() {{add(4);add(5);add(6);}}));//true
+        // 6.集合是否相等
+        System.out.println(collection1.equals(new ArrayList<Integer>() {{add(4);add(5);}}));// false
+        // 7.转成对象数组
+        System.out.println(Arrays.toString(collection1.toArray(new Integer[collection1.size()])));//[4, 5, 6]
+        // 8.获取集合对象的哈希值
+        System.out.println(collection1.hashCode());
+        // 9.返回迭代器对象，用于集合遍历
+        Iterator<Integer> iterator1 = collection1.iterator();// 33796
+        // 10.清空集合
+        collection1.clear();
+        // 11.是否是空集合
+        System.out.println(collection1.isEmpty());// true
+    }
+
+    @Test
+    public void testIterator(){
+        /** Iterator迭代器 */
+        System.out.println("-----------Iterator迭代器-----------");
+        Collection<String> collection2 = new ArrayList<String>(8){{
+            add("a");
+            add("b");
+            add("c");
+            add("d");
+            add("e");
+        }};
+        // 1.获取迭代器
+        Iterator<String> iterator2 = collection2.iterator();
+        // 2.判断是否还有下一个元素
+        // 在调用it.next()方法之前必须要调用it.hasNext()进行检测。
+        // 若不调用，且下一条记录无效，直接调用it.next()会抛出NoSuchElementException异常。
+        while (iterator2.hasNext()){
+//            iterator2.remove(); // 要先调用next()将指针下移，否则抛出异常：java.lang.IllegalStateException
+            // 3.将下一个元素赋给变量a：:①指针下移 ②将下移以后集合位置上的元素返回
+            String str = iterator2.next();
+            System.out.println(str);
+            if(str == "a"){
+                // 4.移除当前元素：会将expectedModCount = modCount
+                iterator2.remove();
+                // 用集合实例的remove，会导致修改数modCount+1，而期望修改数expectedModCount没变
+                // 再次调用next()方法，从而抛出异常：java.util.ConcurrentModificationException
+//                collection2.remove(str);
+            }
+            // 如果是倒数第二个元素删除，不会报错，因为hasNext()方法：:cursor != size;返回了false，不会再进入循环调用next()，不会抛出异常
+//            if ("d".equals(str)){
+//                collection2.remove(str);
+//            }
+            // 如果是最后一个元素删除，hasNext():cursor != size;返回true，因为size减少了一个，之后调用next()，同样抛出异常
+//            if ("e".equals(str)){
+//                collection2.remove(str);
+//            }
+        }
+        System.out.println(collection2); // [b, c, d, e]
+
+        /**foreach循环*/
+        System.out.println("-----------foreach循环-----------");
+        Collection<String> collection3 = new ArrayList<String>(){{
+            add("a");
+            add("b");
+            add("c");
+            add("d");
+            add("e");
+        }};
+        for (String entity : collection3) {
+            System.out.println(entity);
+        }
+        int[] intArray = new int[]{1, 2, 3, 4, 5};
+        for (int entity : intArray) {
+            System.out.println(entity);
+        }
+    }
+
+    @Test
+    public void testList(){
+        /**** List */
+        List<String> list1 = new ArrayList<>();
+        // 1.在index位置插入ele元素
+        list1.add(0, "222");
+        // 2.从index位置开始将所有元素添加进来
+        list1.addAll(1, new ArrayList<String>(){{add("333");add("444");add("555");}});
+        System.out.println(list1); //[222, 333, 444, 555]
+        // 3.获取指定index位置的元素: 555
+        System.out.println(list1.get(3));
+        // 4.返回obj在集合中首次出现的位置：2
+        System.out.println(list1.indexOf("444"));
+        // 5.返回obj在当前集合中末次出现的位置：2
+        System.out.println(list1.lastIndexOf("444"));
+        // 6.移除指定index位置的元素，并返回此元素：444
+        // 注意：如果参数为int，调用该方法；否则调用remove(obj)
+        System.out.println(list1.remove(2));
+        // 7.设置指定index位置的元素为ele：[222, 333, 22222]
+        list1.set(2, "22222");
+        // 8.返回从fromIndex到toIndex位置的子集合：[0,2)：[222, 333]
+        System.out.println(list1.subList(0, 2));
+        System.out.println(list1);
+
+        /****** ArrayList */
+        System.out.println("-----------ArrayList-----------");
+        ArrayList<String> list2 = new ArrayList<>(); // 默认为容量10
+        ArrayList<String> list3 = new ArrayList<>(16);
+
+        /****** LinkedList */
+        System.out.println("-----------LinkedList-----------");
+        //   [cc, ff, dd, ee, cc, ff]
+        LinkedList<String> list4 = new LinkedList<String>(){{add("cc");add("ff");add("dd");add("ee");add("cc");add("ff");}};
+        list4.addFirst("aa"); // [aa, cc, ff, dd, ee, cc, ff]
+        list4.addLast("zz");  // [aa, cc, ff, dd, ee, cc, ff, zz]
+        System.out.println(list4.getFirst()); // aa
+        System.out.println(list4.getLast());  // zz
+        System.out.println(list4.removeFirst());// aa, [cc, ff, dd, ee, cc, ff, zz]
+        System.out.println(list4.removeLast()); // zz, [cc, ff, dd, ee, cc, ff]
+        list4.removeFirstOccurrence("cc"); // 同remove("cc")：[ff, dd, ee, cc, ff]
+        list4.removeLastOccurrence("ff"); // [ff, dd, ee, cc]
+
+
+        /****** Vector */
+        System.out.println("-----------Vector-----------");
+        // [aa, bb, cc, dd]
+        Vector<String> vector1 = new Vector<String>(){{add("aa");add("bb");add("cc");add("dd");}};
+        vector1.addElement("ff"); // [aa, bb, cc, dd, ff]
+        vector1.insertElementAt("kk", 0); // [kk, aa, bb, cc, dd, ff]
+        vector1.setElementAt("jj", 1); // [kk, jj, bb, cc, dd, ff]
+        vector1.removeElement("dd"); // [kk, jj, bb, cc, ff]
+        vector1.removeAllElements();  // 空
+
+        /****** Stack */
+        System.out.println("-----------Stack-----------");
+        Stack<String> stack = new Stack<String>(){{add("aa");add("bb");add("cc");add("dd");}};
+        System.out.println(stack.pop()); // 出栈：dd
+        System.out.println(stack.push("zz"));// 入栈：zz
+        System.out.println(stack.peek());// 栈顶元素： zz
+        System.out.println(stack.empty());// 栈空：false
+        System.out.println(stack.search("aa"));// 元素位置：4
+        System.out.println(stack); // [aa, bb, cc, zz]
+    }
+
+    @Test
+    public void testSet(){
+        /**** Set */
+        /****** HashSet */
+        System.out.println("------------HashSet-------------");
+        HashSet<TestHash> set = new HashSet<>();
+        set.add(new TestHash(11, true));
+        set.add(new TestHash(21, true));
+        set.add(new TestHash(51, true));
+        set.add(new TestHash(31, true));
+        set.add(new TestHash(31, false));
+        System.out.println(set);  // [TestHash{x=31}, TestHash{x=31}, TestHash{x=11}, TestHash{x=21}, TestHash{x=51}]
+
+        /****** LinkedHashSet */
+        System.out.println("-------------LinkedHashSet------------");
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        linkedHashSet.add(new TestHash(11, true));
+        linkedHashSet.add(new TestHash(21, true));
+        linkedHashSet.add(new TestHash(51, true));
+        linkedHashSet.add(new TestHash(31, true));
+        linkedHashSet.add(new TestHash(31, false));
+        System.out.println(linkedHashSet);// [TestHash{x=11}, TestHash{x=21}, TestHash{x=51}, TestHash{x=31}, TestHash{x=31}]
+
+        /****** TreeSet */
+        System.out.println("-------------TreeSet------------");
+        TreeSet<TestHash> treeSet = new TreeSet<>();
+        treeSet.add(new TestHash(5, true));
+        treeSet.add(new TestHash(3, true));
+        treeSet.add(new TestHash(2, true));
+        treeSet.add(new TestHash(31, true));
+        treeSet.add(new TestHash(31, false));
+        // 1.自然排序：按照对象的compareTo方法，[TestHash{x=2}, TestHash{x=3}, TestHash{x=5}, TestHash{x=31}]
+        // 注意：TreeSet根据重写的compareTo方法判断是否相等，如果TestHash只根据x判断，那么new TestHash(31, false)不能插入set中
+        System.out.println(treeSet);
+        System.out.println(treeSet.comparator());
+        System.out.println(treeSet.first()); // 第一个：TestHash{x=2}
+        System.out.println(treeSet.last()); // 最后一个：TestHash{x=31}
+        System.out.println(treeSet.lower(new TestHash(6, true)));// 小于6的第一个：TestHash{x=5}
+        System.out.println(treeSet.higher(new TestHash(4, true))); // 大于4的第一个：TestHash{x=5}
+        // 分隔[x=2, x=5)：[TestHash{x=2}, TestHash{x=3}]
+        System.out.println(treeSet.subSet(new TestHash(2, true), new TestHash(5, true)));
+        System.out.println(treeSet.headSet(new TestHash(5, true)));// 小于5的所有：[TestHash{x=2}, TestHash{x=3}]
+        System.out.println(treeSet.tailSet(new TestHash(5, true)));// 大于等于5的所有：[TestHash{x=5}, TestHash{x=31}]
+
+        // 2.自定义排序：按照Comparator的compare方法，[TestHash{x=31}, TestHash{x=5}, TestHash{x=3}, TestHash{x=2}]
+        // 注意：compare方法相等，那么new TestHash(31, false)不能插入set中
+        TreeSet<TestHash> treeset1 = new TreeSet<>(new Comparator<TestHash>() {
+            @Override
+            public int compare(TestHash o1, TestHash o2) {
+                return (o1.x < o2.x) ? 1 : (((o1.x == o2.x) ? 0 : -1));
+            }
+        });
+        treeset1.add(new TestHash(5, true));
+        treeset1.add(new TestHash(3, true));
+        treeset1.add(new TestHash(2, true));
+        treeset1.add(new TestHash(31, true));
+        treeset1.add(new TestHash(31, false));
+        System.out.println(treeset1);
+    }
+
+    @Test
+    public void testQueue(){
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("aa"); // 入队列：[aa]
+        queue.offer("bb");
+        queue.offer("cc");
+        System.out.println(queue.poll()); //出队列：aa
+        System.out.println(queue.remove());// 移除第一个：bb
+        System.out.println(queue.size()); // 大小：1
+        System.out.println(queue); // [cc]
+    }
+}
--- Java/JavaSE/第11章_Java集合/TestCollections.java
+++ Java/JavaSE/第11章_Java集合/TestCollections.java
@@ -4,195 +4,48 @@ import java.util.*;
 
 public class TestCollections {
     @Test
-    public void testCollection(){
-        /** Collection */
-        System.out.println("-----------Collection-----------");
-        Collection<Integer> collection1 = new ArrayList<>();
+    public void testCollections(){
+        /** Collections工具类 */
+        List<Integer> collection1 = new ArrayList<>();
         // 1.添加
-        collection1.add(1);
-        collection1.add(2);
         collection1.add(3);
-        collection1.add(4);
+        collection1.add(2);
         collection1.add(5);
-        collection1.addAll(new ArrayList<Integer>(){{add(6);}});
-        System.out.println(collection1); // [1, 2, 3, 4, 5, 6]
-        // 2.获取有效元素的个数:6
-        System.out.println(collection1.size());
-        // 3.是否包含某个元素,用元素的equals方法来判断，拿两个集合的元素挨个比较
-        System.out.println(collection1.contains(3));                                      // true
-        System.out.println(collection1.containsAll(new ArrayList<Integer>() {{add(3);}}));// true
-        // 4.删除：通过元素的equals方法判断是否是要删除的那个元素。只会删除找到的第一个元素
-        System.out.println(collection1.remove(3)); // true
-        System.out.println(collection1.removeAll(new ArrayList<Integer>() {{add(2);}})); // true
-        // 5.把交集的结果存在当前集合中，不影响其他集合
-        System.out.println(collection1.retainAll(new ArrayList<Integer>() {{add(4);add(5);add(6);}}));//true
-        // 6.集合是否相等
-        System.out.println(collection1.equals(new ArrayList<Integer>() {{add(4);add(5);}}));// false
-        // 7.转成对象数组
-        System.out.println(Arrays.toString(collection1.toArray(new Integer[collection1.size()])));//[4, 5, 6]
-        // 8.获取集合对象的哈希值
-        System.out.println(collection1.hashCode());
-        // 9.返回迭代器对象，用于集合遍历
-        Iterator<Integer> iterator1 = collection1.iterator();// 33796
-        // 10.清空集合
-        collection1.clear();
-        // 11.是否是空集合
-        System.out.println(collection1.isEmpty());// true
-    }
-
-    @Test
-    public void testIterator(){
-        /** Iterator迭代器 */
-        System.out.println("-----------Iterator迭代器-----------");
-        Collection<String> collection2 = new ArrayList<String>(8){{
-            add("a");
-            add("b");
-            add("c");
-            add("d");
-            add("e");
-        }};
-        // 1.获取迭代器
-        Iterator<String> iterator2 = collection2.iterator();
-        // 2.判断是否还有下一个元素
-        // 在调用it.next()方法之前必须要调用it.hasNext()进行检测。
-        // 若不调用，且下一条记录无效，直接调用it.next()会抛出NoSuchElementException异常。
-        while (iterator2.hasNext()){
-//            iterator2.remove(); // 要先调用next()将指针下移，否则抛出异常：java.lang.IllegalStateException
-            // 3.将下一个元素赋给变量a：:①指针下移 ②将下移以后集合位置上的元素返回
-            String str = iterator2.next();
-            System.out.println(str);
-            if(str == "a"){
-                // 4.移除当前元素：会将expectedModCount = modCount
-                iterator2.remove();
-                // 用集合实例的remove，会导致修改数modCount+1，而期望修改数expectedModCount没变
-                // 再次调用next()方法，从而抛出异常：java.util.ConcurrentModificationException
-//                collection2.remove(str);
+        collection1.add(4);
+        collection1.add(1);
+        System.out.println(collection1); // [3, 2, 5, 4, 1]
+        Collections.reverse(collection1); // 反转 List 中元素的顺序：[1, 4, 5, 2, 3]
+        Collections.shuffle(collection1); // 对 List 集合元素进行随机排序：[4, 1, 3, 5, 2]
+        Collections.sort(collection1); // 自然顺序，按升序排序：[1, 2, 3, 4, 5]
+        Collections.sort(collection1, new Comparator<Integer>() { // 自定义Comparator排序：[5, 4, 3, 2, 1]
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 >= o2) ? -1 : 1;
             }
-            // 如果是倒数第二个元素删除，不会报错，因为hasNext()方法：:cursor != size;返回了false，不会再进入循环调用next()，不会抛出异常
-//            if ("d".equals(str)){
-//                collection2.remove(str);
-//            }
-            // 如果是最后一个元素删除，hasNext():cursor != size;返回true，因为size减少了一个，之后调用next()，同样抛出异常
-//            if ("e".equals(str)){
-//                collection2.remove(str);
-//            }
-        }
-        System.out.println(collection2); // [b, c, d, e]
-
-        /**foreach循环*/
-        System.out.println("-----------foreach循环-----------");
-        Collection<String> collection3 = new ArrayList<String>(){{
-            add("a");
-            add("b");
-            add("c");
-            add("d");
-            add("e");
-        }};
-        for (String entity : collection3) {
-            System.out.println(entity);
-        }
-        int[] intArray = new int[]{1, 2, 3, 4, 5};
-        for (int entity : intArray) {
-            System.out.println(entity);
-        }
-    }
-
-    @Test
-    public void testList(){
-        /**** List */
-        List<String> list1 = new ArrayList<>();
-        // 1.在index位置插入ele元素
-        list1.add(0, "222");
-        // 2.从index位置开始将所有元素添加进来
-        list1.addAll(1, new ArrayList<String>(){{add("333");add("444");add("555");}});
-        System.out.println(list1); //[222, 333, 444, 555]
-        // 3.获取指定index位置的元素: 555
-        System.out.println(list1.get(3));
-        // 4.返回obj在集合中首次出现的位置：2
-        System.out.println(list1.indexOf("444"));
-        // 5.返回obj在当前集合中末次出现的位置：2
-        System.out.println(list1.lastIndexOf("444"));
-        // 6.移除指定index位置的元素，并返回此元素：444
-        // 注意：如果参数为int，调用该方法；否则调用remove(obj)
-        System.out.println(list1.remove(2));
-        // 7.设置指定index位置的元素为ele：[222, 333, 22222]
-        list1.set(2, "22222");
-        // 8.返回从fromIndex到toIndex位置的子集合：[0,2)：[222, 333]
-        System.out.println(list1.subList(0, 2));
-        System.out.println(list1);
-
-        /****** ArrayList */
-        System.out.println("-----------ArrayList-----------");
-        ArrayList<String> list2 = new ArrayList<>(); // 默认为容量10
-        ArrayList<String> list3 = new ArrayList<>(16);
-
-        /****** LinkedList */
-        System.out.println("-----------LinkedList-----------");
-        //   [cc, ff, dd, ee, cc, ff]
-        LinkedList<String> list4 = new LinkedList<String>(){{add("cc");add("ff");add("dd");add("ee");add("cc");add("ff");}};
-        list4.addFirst("aa"); // [aa, cc, ff, dd, ee, cc, ff]
-        list4.addLast("zz");  // [aa, cc, ff, dd, ee, cc, ff, zz]
-        System.out.println(list4.getFirst()); // aa
-        System.out.println(list4.getLast());  // zz
-        System.out.println(list4.removeFirst());// aa, [cc, ff, dd, ee, cc, ff, zz]
-        System.out.println(list4.removeLast()); // zz, [cc, ff, dd, ee, cc, ff]
-        list4.removeFirstOccurrence("cc"); // 同remove("cc")：[ff, dd, ee, cc, ff]
-        list4.removeLastOccurrence("ff"); // [ff, dd, ee, cc]
+        }); //
+        Collections.swap(collection1, 1, 2); // 交换下标元素： [5, 3, 4, 2, 1]
+        System.out.println(Collections.max(collection1)); // 自然顺序，返回给定集合中的最大元素: 5
+        // 根据 Comparator 指定的顺序，返回给定集合中的最大元素: 1
+        System.out.println(Collections.max(collection1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 >= o2) ? -1 : 1;
+            }
+        }));
+        System.out.println(Collections.min(collection1)); // 自然顺序，返回给定集合中的最小元素: 1
+        // 根据 Comparator 指定的顺序，返回给定集合中的最小元素: 5
+        System.out.println(Collections.min(collection1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 >= o2) ? -1 : 1;
+            }
+        }));
+        System.out.println(Collections.frequency(collection1, 3));// 返回指定集合中指定元素的出现次数：1
+        List<Integer> collection2 = new ArrayList<Integer>(){{add(3);}};
+        Collections.copy(collection1, collection2); // 将collection2中的内容复制到collection1中：[3, 3, 4, 2, 1]
+        Collections.replaceAll(collection1, 2, 1);// 使用新值替换 List 对象的所有旧值：[5, 3, 4, 1, 1]
 
 
-        /****** Vector */
-        // [aa, bb, cc, dd]
-        Vector<String> vector1 = new Vector<String>(){{add("aa");add("bb");add("cc");add("dd");}};
-        vector1.addElement("ff"); // [aa, bb, cc, dd, ff]
-        vector1.insertElementAt("kk", 0); // [kk, aa, bb, cc, dd, ff]
-        vector1.setElementAt("jj", 1); // [kk, jj, bb, cc, dd, ff]
-        vector1.removeElement("dd"); // [kk, jj, bb, cc, ff]
-        vector1.removeAllElements();  // 空
-
-    }
-
-    @Test
-    public void testSet(){
-        /**** Set */
-        /****** HashSet */
-        System.out.println("------------HashSet-------------");
-        HashSet<TestHash> set = new HashSet<>();
-        set.add(new TestHash(11, true));
-        set.add(new TestHash(21, true));
-        set.add(new TestHash(51, true));
-        set.add(new TestHash(31, true));
-        set.add(new TestHash(31, false));
-        System.out.println(set);  // [TestHash{x=31}, TestHash{x=31}, TestHash{x=11}, TestHash{x=21}, TestHash{x=51}]
-
-        /****** LinkedHashSet */
-        System.out.println("-------------LinkedHashSet------------");
-        LinkedHashSet linkedHashSet = new LinkedHashSet();
-        linkedHashSet.add(new TestHash(11, true));
-        linkedHashSet.add(new TestHash(21, true));
-        linkedHashSet.add(new TestHash(51, true));
-        linkedHashSet.add(new TestHash(31, true));
-        linkedHashSet.add(new TestHash(31, false));
-        System.out.println(linkedHashSet);// [TestHash{x=11}, TestHash{x=21}, TestHash{x=51}, TestHash{x=31}, TestHash{x=31}]
-
-        /****** TreeSet */
-        System.out.println("-------------TreeSet------------");
-        TreeSet<TestHash> treeSet = new TreeSet<>();
-        treeSet.add(new TestHash(5, true));
-        treeSet.add(new TestHash(3, true));
-        treeSet.add(new TestHash(2, true));
-        treeSet.add(new TestHash(31, true));
-        treeSet.add(new TestHash(31, false));
-        // 自动排序：按照对象的compareTo方法，[TestHash{x=2}, TestHash{x=3}, TestHash{x=5}, TestHash{x=31}]
-        // 注意：TreeSet根据重写的compareTo方法判断是否相等，如果TestHash只根据x判断，那么new TestHash(31, false)不能插入set中
-        System.out.println(treeSet);
-        System.out.println(treeSet.comparator());
-        System.out.println(treeSet.first()); // 第一个：TestHash{x=2}
-        System.out.println(treeSet.last()); // 最后一个：TestHash{x=31}
-        System.out.println(treeSet.lower(new TestHash(6, true)));// 小于6的第一个：TestHash{x=5}
-        System.out.println(treeSet.higher(new TestHash(4, true))); // 大于4的第一个：TestHash{x=5}
-        // 分隔[x=2, x=5)：[TestHash{x=2}, TestHash{x=3}]
-        System.out.println(treeSet.subSet(new TestHash(2, true), new TestHash(5, true)));
-        System.out.println(treeSet.headSet(new TestHash(5, true)));// 小于5的所有：[TestHash{x=2}, TestHash{x=3}]
-        System.out.println(treeSet.tailSet(new TestHash(5, true)));// 大于等于5的所有：[TestHash{x=5}, TestHash{x=31}]
+        Collections.synchronizedList(collection2)
     }
 }
